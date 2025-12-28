3f823662450ae8f8f0d0113831f154441fb9746e
hacker12345227 <Jarno.eulen12@gmail.com>
2025-12-28 10:39:09 +0100

    Update users.js

--- users.js
+++ users.js
@@ -1,11 +1,4 @@
-// users.js
-// LET OP: dit is client-side, niet superveilig, maar WEL gratis & stabiel
-
 const USERS = [
-  { username: "admin", password: "1234" },
-  { username: "jarno", password: "0000" },
-  { username: "test",  password: "test123" }
-];
-
-// 👉 Voeg nieuwe users zo toe:
-// { username: "naam", password: "wachtwoord" },
+  { username:"admin", password:"1234" },
+  { username:"test", password:"test" }
+]
