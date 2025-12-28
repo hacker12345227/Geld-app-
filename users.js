5dc2b39d2eb06d8e7d06207ebde687735c756c0f
hacker12345227 <Jarno.eulen12@gmail.com>
2025-12-28 10:24:19 +0100

    Create users.js

---
+++ users.js
@@ -0,0 +1,8 @@
+// users.js
+// Voeg hier alle accounts toe
+// Wachtwoorden simpel als string (niet veilig, maar voldoende voor privé gebruik)
+const USERS = [
+  { username: "Jarno", password: "13459" },
+  { username: "Alice", password: "abcd" },
+  { username: "Bob", password: "1234" }
+];
