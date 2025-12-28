69e5f02316d04c93819b87bae5c352235e082286
hacker12345227 <Jarno.eulen12@gmail.com>
2025-12-28 10:14:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
-
 self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open("zykon").then(c=>
-      c.addAll(["./","index.html","style.css","app.js"])
-    )
+    caches.open("zykon").then(c=>c.addAll(["./","index.html","style.css","app.js"]))
   )
 })
 
 self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  )
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
 })
